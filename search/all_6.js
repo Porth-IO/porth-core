9527046202ba63bf7c31341885cc53ed4b3976bb
c21051997 <c21051997@users.noreply.github.com>
2026-03-07 16:32:36 +0000

    deploy: ff32175e4c5efe230530b0c5d67de9968c948ec6

--- search/all_6.js
+++ search/all_6.js
@@ -1,15 +1,14 @@
 var searchData=
 [
   ['cache_5fline_5falignment_0',['cache_line_alignment',['../namespaceporth.html#a6da83c2b0b7c8e9583de6c77238c3a86',1,'porth']]],
-  ['cache_5fline_5fsize_1',['CACHE_LINE_SIZE',['../namespaceporth.html#aea2007160ca0f9d6240e2d0e0fbc3fbc',1,'porth']]],
-  ['calibrate_5fclock_2ecpp_2',['calibrate_clock.cpp',['../calibrate__clock_8cpp.html',1,'']]],
-  ['capabilities_3',['🌟 Core Capabilities',['../md_README.html#autotoc_md7',1,'']]],
-  ['cardiffchip_4',['CardiffChip',['../namespaceCardiffChip.html',1,'']]],
-  ['compound_20semiconductors_5',['Porth-IO: The Sovereign Logic Layer for Compound Semiconductors',['../md_README.html',1,'']]],
-  ['control_6',['control',['../structporth_1_1PorthDeviceLayout.html#a7ad48a4b73de74094b0fb8634940fe8c',1,'porth::PorthDeviceLayout']]],
-  ['core_20capabilities_7',['🌟 Core Capabilities',['../md_README.html#autotoc_md7',1,'']]],
-  ['counter_8',['counter',['../structporth_1_1PorthDeviceLayout.html#ab0d2c4e602d258db4649cb6b30d09f72',1,'porth::PorthDeviceLayout']]],
-  ['ctrl_5fidle_9',['CTRL_IDLE',['../namespaceCardiffChip.html#ac554ac2ef90a347a4ccefb0bcd92e5e5',1,'CardiffChip']]],
-  ['ctrl_5freset_10',['CTRL_RESET',['../namespaceCardiffChip.html#a6c5e186a1db54869615005428cccc173',1,'CardiffChip']]],
-  ['ctrl_5fstart_11',['CTRL_START',['../namespaceCardiffChip.html#a7df4eed90f1bdfc897514707c8bd5074',1,'CardiffChip']]]
+  ['calibrate_5fclock_2ecpp_1',['calibrate_clock.cpp',['../calibrate__clock_8cpp.html',1,'']]],
+  ['capabilities_2',['🌟 Core Capabilities',['../md_README.html#autotoc_md7',1,'']]],
+  ['cardiffchip_3',['CardiffChip',['../namespaceCardiffChip.html',1,'']]],
+  ['compound_20semiconductors_4',['Porth-IO: The Sovereign Logic Layer for Compound Semiconductors',['../md_README.html',1,'']]],
+  ['control_5',['control',['../structporth_1_1PorthDeviceLayout.html#a7ad48a4b73de74094b0fb8634940fe8c',1,'porth::PorthDeviceLayout']]],
+  ['core_20capabilities_6',['🌟 Core Capabilities',['../md_README.html#autotoc_md7',1,'']]],
+  ['counter_7',['counter',['../structporth_1_1PorthDeviceLayout.html#ab0d2c4e602d258db4649cb6b30d09f72',1,'porth::PorthDeviceLayout']]],
+  ['ctrl_5fidle_8',['CTRL_IDLE',['../namespaceCardiffChip.html#ac554ac2ef90a347a4ccefb0bcd92e5e5',1,'CardiffChip']]],
+  ['ctrl_5freset_9',['CTRL_RESET',['../namespaceCardiffChip.html#a6c5e186a1db54869615005428cccc173',1,'CardiffChip']]],
+  ['ctrl_5fstart_10',['CTRL_START',['../namespaceCardiffChip.html#a7df4eed90f1bdfc897514707c8bd5074',1,'CardiffChip']]]
 ];
--- search/variables_a.js
+++ search/variables_a.js
@@ -4,5 +4,6 @@ var searchData=
   ['percentile_5fp99_5f9_1',['PERCENTILE_P99_9',['../namespaceporth.html#acb2f09b17c551cd6876db4bc5a8ec5cd',1,'porth']]],
   ['percentile_5fp99_5f99_2',['PERCENTILE_P99_99',['../namespaceporth.html#a692bcf7caf30e80dcf9f224e574d9f8c',1,'porth']]],
   ['percentile_5fq1_3',['PERCENTILE_Q1',['../namespaceporth.html#af9a594d3bc323379ff3350c39efcd189',1,'porth']]],
-  ['percentile_5fq3_4',['PERCENTILE_Q3',['../namespaceporth.html#ab38ad03f9855da59a07b22888ec51e03',1,'porth']]]
+  ['percentile_5fq3_4',['PERCENTILE_Q3',['../namespaceporth.html#ab38ad03f9855da59a07b22888ec51e03',1,'porth']]],
+  ['porth_5fcache_5fline_5fsize_5',['PORTH_CACHE_LINE_SIZE',['../namespaceporth.html#af0445d9d65f5f06a472597d6bfdb26a1',1,'porth']]]
 ];
